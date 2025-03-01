f75cf972e89a6dac20cfef77523b049c43b57ab9
Javier Chung <34190746+Javiebear@users.noreply.github.com>
2025-02-28 22:48:52 -0500

    Update imageProducer.py

--- pubsub/imageProducer.py
+++ pubsub/imageProducer.py
@@ -17,7 +17,7 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=files[0];
 project_id="directed-post-449003-u2";
 
 # publisher for the topic adhearing to the car view data images of pedestrians
-topic_name = "car_image_view"; 
+topic_name = "car_view_image"; 
 publisher = pubsub_v1.PublisherClient()
 topic_path = publisher.topic_path(project_id, topic_name)
 
@@ -26,22 +26,32 @@ df=pd.read_csv('Labels.csv')
 # going through every image
 for _, row in df.iterrows():
     image = row["Occluded_Image_view"]
-    # opening the image and publishing it
-    try:
-        # reading the image and coverting it to hex
-        with open(f"{IMAGE_PATH}/{image}", "rb") as f:
-            image_bytes = f.read()
-            image_hex = image_bytes.hex()
 
-        message = json.dumps({"image": image_hex, "id": image}).encode("utf-8")
+    print(image)
+
+    # creating the image path
+    imagepath = os.path.join(IMAGE_PATH, image)
+
+    if os.path.exists(imagepath):
+
+        # opening the image and publishing it
+        try:
+            # reading the image and coverting it to hex
+            with open(imagepath, "rb") as f:
+                image_bytes = f.read()
+                image_hex = image_bytes.hex()
+
+            message = json.dumps({"image": image_hex, "id": image}).encode("utf-8")
 
 
-        future = publisher.publish(topic_path, message);
-        #ensure that the publishing has been completed successfully
-        future.result()    
-        print("The messages has been published successfully")
-    except: 
-        print("Failed to publish the message")
+            future = publisher.publish(topic_path, message);
+            #ensure that the publishing has been completed successfully
+            future.result()    
+            print("The messages has been published successfully")
+        except: 
+            print("Failed to publish the message")
+    else:
+        print("No image found.")
 
 
 
